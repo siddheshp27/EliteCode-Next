447ae559325df66a58bb91f29f5bc928cb5805f2
aryanp86 <aryan.pokharkar21@vit.edu>
2024-04-29 20:53:33 +0530

    Modified home page

---
+++ app/Components/Navbar.jsx
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+import { FloatingNav } from "../ui/floating-navbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+export function Navbar() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-white" />,
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <IconUser className="h-4 w-4 text-white" />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      icon: <IconMessage className="h-4 w-4 text-white" />,
+    },
+  ];
+  return (
+    <div className="relative text-white w-full">
+      <FloatingNav navItems={navItems} />
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 "use client";
 import React from "react";
 import { useState, useEffect } from "react";
-import NavMenu from "./Components/NavMenu";
 import { useAuth0, Auth0Provider } from "@auth0/auth0-react";
 import ProblemPage from "./Components/ProblemPage";
+import Home from "./Components/Home";
 
 const App = () => {
   return (
@@ -14,8 +14,8 @@ const App = () => {
         redirect_uri: "http://localhost:3000/",
       }}
     >
-      <div className="main h-screen w-full">
-        <NavMenu />
+      <div className="main w-full overflow-clip">
+        <Home />
       </div>
     </Auth0Provider>
   );
